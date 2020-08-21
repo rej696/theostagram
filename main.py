bae0d99265a4502382675f7b60a7a6631ef16d05
Rowan_Surface <55083730+rej696@users.noreply.github.com>
2020-08-22 00:42:59 +0100

    create template, update stylesheet, update main to serve home template

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:red;'>Hello There!</h1>"
+def home():
+    style = url_for('static', filename='style.css')
+    return render_template('home.html', style=style)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
